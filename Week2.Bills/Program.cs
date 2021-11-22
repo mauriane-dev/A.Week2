ff47bb5c89687bce92cbbba0b7f9edb65ea66804
Alessandra Degan Di Dieco <alessandra.degan@icubed.it>
2021-11-22 11:17:53 +0100

    Aggiungere i file di progetto.

---
+++ Week2.Bills/Program.cs
@@ -0,0 +1,40 @@
+﻿//Realizzare un’applicazione console che consenta
+//di gestire le bollette di un utente.
+
+//La bolletta ha Quota fissa (40 €), kWh consumati,
+//Importo totale e Utente (intestatario).
+
+//Inizializziamo un array con alcuni codici fiscali. 
+
+//All'accesso, l'utente visualizza un menu
+//in cui può scegliere di:
+//1- calcolare la sua bolletta
+//2- visualizzare le sue bollette
+//3- uscire dal programma.
+
+//1-calcolare la bolletta
+//L'utente deve inserire il suo Codice Fiscale.
+
+//SE il codice fiscale è tra quelli nell'array,
+//L'utente inserisce i kW/h consumati, il suo nome
+//e il suo cognome.
+//A questo punto, viene calcolata la bolletta.
+//L'importo della bolletta è dato da:
+//quota fissa (40€) + kwH consumati * 10
+//Una volta calcolata, stampare a video e
+//su un file txt le informazioni della bolletta,
+//codice fiscale, nome, cognome, kWh e importo da pagare.
+
+//ALTRIMENTI, a video compare: 'Cliente non trovato'
+//e si torna al menu principale.
+
+//2- visualizzare le bollette,
+//L'utente deve inserire il suo cf.
+
+//SE il codice fiscale è tra quelli nell'array,
+//recuperare il contenuto del file
+//e mostrare a video SOLO le bollette associate
+//al codice fiscale dell'utente
+
+//ALTRIMENTI
+//'Cliente non trovato'
---
+++ Week2.FileSystem/Program.cs
@@ -0,0 +1,45 @@
+﻿//Lettura/scrittura da/su file
+
+//StreamWriter
+//StreamReader
+
+//Scrivere su file
+string path = @"C:\Users\Utente\Desktop\A.Week2\Week2\Week2.FileSystem\prova.txt";
+
+StreamWriter sw = new StreamWriter(path);
+
+sw.WriteLine("Prova di scrittura");
+
+sw.Close();
+
+//se non specifico un path completo, il file viene creato in bin -> Debug -> .net6 -> qui trovo il file
+StreamWriter sw3 = new StreamWriter(@"prova2.txt");
+
+sw3.WriteLine("Prova di scrittura");
+
+sw3.Close();
+
+using (StreamWriter sw2 = new StreamWriter(path, true))
+{
+    sw2.WriteLine("Scrittura su file");
+}
+
+using (StreamReader sr = new StreamReader(path))
+{
+    string text = sr.ReadToEnd(); //legge tutto il contenuto del file
+}
+
+using (StreamReader sr2 = new StreamReader(path))
+{
+    string line = sr2.ReadLine(); //leggere una riga 
+}
+
+using (StreamReader sr3 = new StreamReader(path))
+{
+    string text = sr3.ReadToEnd();
+    string[] lines = text.Split("\r\n");
+}
+
+
+
+
